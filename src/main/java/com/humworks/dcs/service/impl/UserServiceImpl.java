023280baac62933568a958a46edd39bcc1a94320
unknown <Sebin@HS-PC07.Office.Hummingsoft.com.my>
2016-12-21 18:41:10 +0800

    Signed-off-by: unknown <Sebin@HS-PC07.Office.Hummingsoft.com.my>

--- src/main/java/com/humworks/dcs/service/impl/UserServiceImpl.java
+++ src/main/java/com/humworks/dcs/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.humworks.dcs.service.impl;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,17 +15,23 @@ import com.humworks.dcs.service.UserService;
 
 @Service("userService")
 @Transactional
+@PropertySource(value={"classpath:constants.properties"})
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserDao userDao;
 	
 	@Autowired
+	private Environment environment;
+	
+	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
 	@Override
 	public void save(User user) {
-	       user.setStrPassword(passwordEncoder.encode(user.getStrPassword()));
+	       user.setStrPassword(passwordEncoder.encode(environment.getProperty("default_password")));
+	       user.setIntCreatedBy(1);
+	       user.setIntModifiedBy(1);
 	       userDao.save(user);
 	}
 
